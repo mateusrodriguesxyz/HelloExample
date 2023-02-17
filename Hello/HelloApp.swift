2ec3cc0e55ec0b19f3c5b703f0657c2c176837f5
Mateus Rodrigues <mateus.fr26@gmail.com>
2023-02-17 17:25:08 -0300

    Initial Commit

---
+++ Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by Mateus on 17/02/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
